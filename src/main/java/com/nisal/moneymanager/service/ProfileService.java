11a3590d8686e7e529f1bc54a8f906bceb96dc12
NisalSandaru <nisalsandaru1@gmail.com>
2025-10-24 13:16:07 +0530

    All done

--- src/main/java/com/nisal/moneymanager/service/ProfileService.java
+++ src/main/java/com/nisal/moneymanager/service/ProfileService.java
@@ -7,6 +7,7 @@ import com.nisal.moneymanager.mapper.ProfileMapper;
 import com.nisal.moneymanager.repository.ProfileRepository;
 import com.nisal.moneymanager.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -28,6 +29,9 @@ public class ProfileService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    @Value("${app.activation.url}")
+    private String activationURL;
+
     public ProfileDTO registerProfile(ProfileDTO profileDTO) {
         ProfileEntity newProfile = ProfileMapper.toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
@@ -35,7 +39,7 @@ public class ProfileService {
         newProfile = profileRepository.save(newProfile);
 
         //send activation email
-        String activationLink = "http://localhost:8080/api/v1.0/activate?token=" + newProfile.getActivationToken();
+        String activationLink = activationURL+"/api/v1.0/activate?token=" + newProfile.getActivationToken();
         String subject = "Activate your Money Manager Account";
         String body = "Click on the following link to activate your Account: " + activationLink;
         emailService.sendEmail(newProfile.getEmail(), subject, body);
